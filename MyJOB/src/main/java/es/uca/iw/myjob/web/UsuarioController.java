47e2e6c158b223edd1b41d84e744e9ed3e77f2c9
Manolo <fildru19@gmail.com>
2016-05-21 17:36:02 +0200

    Añadido jQuery, datatables y bootstrap

--- MyJOB/src/main/java/es/uca/iw/myjob/web/UsuarioController.java
+++ MyJOB/src/main/java/es/uca/iw/myjob/web/UsuarioController.java
@@ -3,9 +3,13 @@ import es.uca.iw.myjob.domain.Usuario;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.gvnix.addon.web.mvc.annotations.jquery.GvNIXWebJQuery;
+import org.gvnix.addon.datatables.annotations.GvNIXDatatables;
 
 @RequestMapping("/usuarios")
 @Controller
 @RooWebScaffold(path = "usuarios", formBackingObject = Usuario.class)
+@GvNIXWebJQuery
+@GvNIXDatatables(ajax = true)
 public class UsuarioController {
 }
